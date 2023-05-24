4c35f34a2406730a89620d4a44abad868b7343c8
desydwipuspita <118228888+desydwipuspita@users.noreply.github.com>
2023-05-24 20:39:17 +0700

    Add files via upload

---
+++ webdesycrud/about.php
@@ -0,0 +1,19 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Mochi adalah kue Jepang yang terbuat dari beras ketan, ditumbuk sehingga lembut dan lengket, kemudian dibentuk menjadi bulat</p>
+<p>
+Disini anda dapat membeli mochi dengan berbagai varian rasa 
+</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ webdesycrud/footer.php
@@ -0,0 +1,32 @@
+
+	<footer style="border-top: 4px solid #c12e2a;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #c12e2a"><b></b></h3>
+					
+					<p><i class="glyphicon glyphicon-earphone"></i> +6281553257587</p>
+					<p>Kota Malang</p>
+				
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href="produk.php"  style="color: #000">Produk</a></p>
+					<p><a href="about.php"  style="color: #000">Tentang kami</a></p>
+					<p><a href="https://instagram.com/desssyys?igshid=MzRlODBiNWFlZA=="  style="color: #000" target="_blank">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #c12e2a; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; DESSSYYS</span>
+		</div>
+	</footer>
+
+</body>
+</html>
